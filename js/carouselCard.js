e4c50362cf0db67b5e5a5042a29435e365ea4eee
TheProductX <114508960+TheProductX@users.noreply.github.com>
2022-09-27 22:02:53 +0530

    Initial Commit

---
+++ js/carouselCard.js
@@ -0,0 +1,145 @@
+const template = document.createElement("template");
+template.innerHTML = `
+
+  <style>
+    * {
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+    }
+    
+    .card {
+      position: relative;
+      height: 35rem;
+      border-top-left-radius: 50px;
+      border-bottom-right-radius: 50px;
+      overflow: hidden;
+      background-size: cover;
+      display: flex;
+      align-items: end;
+      background-size: cover;
+      background-position: center;
+      background-repeat: no-repeat;
+      background-size: cover;
+      box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
+    }
+
+    .card .content {
+      display: flex;
+      justify-content: space-between;
+      width: 100%;
+      padding: 1.5rem 1rem;
+      backdrop-filter: blur(16px) saturate(158%);
+      -webkit-backdrop-filter: blur(25px) saturate(158%);
+      background-color: rgba(66, 64, 70, 0.8);
+      border: 1px solid rgba(255, 255, 255, 0.125);
+      border-top-left-radius: 15px;
+      border-top-right-radius: 15px;
+      flex-direction: column;
+      gap: 1.2rem;
+    }
+
+    .content .profile-name {
+      font-size: 1.9rem;
+      font-weight: 600;
+      color: #fff;
+      text-transform: uppercase;
+    }
+
+    .content .institute-name {
+      font-size: 1.4rem;
+      font-weight: 400;
+      color: rgb(235, 235, 235);
+    }
+
+    .content .profile-header {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+    }
+
+    .profile-description {
+      display: flex;
+      flex-direction: column;
+      gap: 0.8rem;
+    }
+
+    .company {
+      display: flex;
+      justify-content: flex-start;
+      align-items: center;
+      gap: 1rem;
+    }
+
+    .profile-role {
+      font-size: 1.6rem;
+      color: #fff;
+    }
+
+    .company-logo {
+      height: 3.5rem;
+      width: 7.5rem;
+      background-color: #fff;
+      padding: 0.5rem;
+      border-radius: 10px;
+      object-fit: conver;
+    }
+
+    .company-logo:hover {
+      transform: scale(1.5);
+      transition: 0.5s ease-in;
+    }
+
+    ion-icon {
+      color: white;
+    }
+
+  </style>
+
+  <div class="card">
+    <div class="content">
+      <div class="profile-header">
+        <div class="profile-description">
+          <h2 class="profile-name"><slot name="profile-name"></h2>
+          <p class="institute-name"><slot name="institute-name"></p>
+        </div>
+        <a target="_blank" class="linkedin">
+          <ion-icon size="large" name="logo-linkedin"></ion-icon>
+        </a>
+      </div>
+      <div class="company">
+        <img class="company-logo" alt="company logo"/>
+        <p class="profile-role"><slot name="profile-role"></p>
+      </div>
+    </div>
+  </div>
+`;
+
+class CarouselCard extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
+  }
+
+  connectedCallback() {
+    const profileImage = this.getAttribute("profile-image");
+    const companyLogo = this.getAttribute("company-logo");
+
+    if (profileImage) {
+      this.shadowRoot.querySelector(
+        ".card"
+      ).style.backgroundImage = `url(${this.getAttribute("profile-image")})`;
+    }
+
+    this.shadowRoot.querySelector(".linkedin").href =
+      this.getAttribute("linkedin-url") ?? "https://www.linkedin.com";
+
+    if (companyLogo) {
+      this.shadowRoot.querySelector(".company-logo").src =
+        this.getAttribute("company-logo");
+    }
+  }
+}
+
+window.customElements.define("carousel-card", CarouselCard);
---
+++ js/script.js
@@ -0,0 +1,127 @@
+const swiperProfile = new Swiper(".swiper-profile", {
+  slidesPerView: 4,
+  spaceBetween: 25,
+  loop: true,
+  centerSlide: "true",
+  fade: "true",
+  grabCursor: "true",
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    520: {
+      slidesPerView: 2,
+    },
+    950: {
+      slidesPerView: 3,
+    },
+    1300: {
+      slidesPerView: 4,
+    },
+  },
+});
+
+const swiperInstitution = new Swiper(".swiper-institution", {
+  slidesPerView: 7,
+  speed: 1500,
+  spaceBetween: 25,
+  loop: true,
+  centerSlide: "true",
+  fade: "true",
+  allowTouchMove: false,
+  autoplay: {
+    delay: 1,
+    pauseOnMouseEnter: false,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 3,
+    },
+    520: {
+      slidesPerView: 5,
+    },
+    950: {
+      slidesPerView: 6,
+    },
+    1300: {
+      slidesPerView: 7,
+    },
+  },
+});
+
+// Show current year
+const yearElement = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+yearElement.textContent = currentYear.toString();
+
+// Make mobile navigation work
+const btnNav = document.querySelector(".btn-mobile-nav");
+const header = document.querySelector(".header");
+
+btnNav.addEventListener("click", () => {
+  header.classList.toggle("nav-open");
+});
+
+// Smooth scroll
+// Smooth scroll behavior
+const allLinks = document.querySelectorAll("a:link");
+allLinks.forEach((link) => {
+  if (link.id === "unlink-active") return;
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // scroll back to top
+    if (href === "#") {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
+
+    // Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionElement = document.querySelector(href);
+      sectionElement.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // Close mobile navigation
+    if (link.classList.contains("main-nav-link")) {
+      header.classList.remove("nav-open");
+    }
+  });
+});
+
+// Sticky navigation
+const sectionHero = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const [ent] = entries;
+    if (!ent.isIntersecting) {
+      console.log("Not Intersecting");
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting) {
+      console.log("Intersecting");
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHero);
